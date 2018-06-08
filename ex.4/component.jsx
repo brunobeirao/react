039291c9b578116181739c8bd2131f2d7ea9d5ca
bsbeirao <bsbeirao@gmail.com>
2018-06-07 21:32:47 -0300

    ex3 e ex4

---
+++ ex.4/component.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default props => (
+    <h1>1º componente</h1>
+)
+
+export const Segundo = props => (
+    <h1>2º componente</h1>
+)
+
+//export {Primeiro, Segundo}
